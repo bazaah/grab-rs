6c2f3d482098533cb288d765097dbda250ff1eef
Bazaah <github@luxolus.com>
2021-01-31 19:35:41 +0000

    add Input + Config + Buider
    These three can be though of as stages in the main functionality of this
    library. Each is separated by a fallible stage, the flow looks something
    like:
    
    Builder --> Config --> Input --> InputReader
            ^^         ^^        ^^------ access the input
            |          |----------- parse the input
            |
            validate the config

---
+++ src/builder.rs
@@ -0,0 +1,162 @@
+use crate::{
+    error::input::InputError,
+    input::Input,
+    parsers::{File, InputType, Parser, Stdin, Text},
+};
+
+use std::{ffi::OsStr, fmt};
+
+#[derive(Debug, Clone, Default)]
+pub struct Builder {
+    stdin: Option<Stdin>,
+    file: Option<File>,
+    text: Option<Text>,
+}
+
+impl Builder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with<F>(self, f: F) -> Self
+    where
+        F: FnMut(&mut Self) -> &mut Self,
+    {
+        let mut this = self;
+        let mut actions = f;
+
+        actions(&mut this);
+
+        this
+    }
+
+    pub fn build(self) -> Result<Config, Self> {
+        if self.is_valid() {
+            return Ok(Config { inner: self });
+        }
+
+        Err(self)
+    }
+
+    pub fn text(&mut self) -> &mut Self {
+        self.with_text(Text::new())
+    }
+
+    pub fn with_text(&mut self, t: Text) -> &mut Self {
+        self.text = Some(t);
+
+        self
+    }
+
+    pub fn stdin(&mut self) -> &mut Self {
+        self.with_stdin(Stdin::new())
+    }
+
+    pub fn with_stdin(&mut self, s: Stdin) -> &mut Self {
+        self.stdin = Some(s);
+
+        self
+    }
+
+    pub fn file(&mut self) -> &mut Self {
+        self.with_file(File::new())
+    }
+
+    pub fn with_file(&mut self, f: File) -> &mut Self {
+        self.file = Some(f);
+
+        self
+    }
+
+    pub fn is_valid(&self) -> bool {
+        let b = self;
+
+        b.text.is_some() || b.stdin.is_some() || b.file.is_some()
+    }
+}
+
+#[derive(Clone)]
+pub struct Config {
+    inner: Builder,
+}
+
+impl Config {
+    pub fn parse(&self, input: &str) -> Result<Input, InputError> {
+        self.parse_str(input).map(|t| Input::from_input_type(t))
+    }
+
+    pub fn parse_os(&self, input: &OsStr) -> Result<Input, InputError> {
+        self.parse_os_str(input).map(|t| Input::from_input_type(t))
+    }
+
+    fn internal_parse<F>(&self, mut f: F) -> Result<InputType, InputError>
+    where
+        F: FnMut(&dyn Parser) -> Result<InputType, InputError>,
+    {
+        let b = &self.inner;
+        let list = [
+            b.file.as_ref().map(|p| p as &dyn Parser),
+            b.stdin.as_ref().map(|p| p as &dyn Parser),
+            b.text.as_ref().map(|p| p as &dyn Parser),
+        ];
+        let mut error: Option<InputError> = None;
+
+        for parser in list.iter().filter_map(|o| *o) {
+            match f(parser) {
+                Ok(success) => return Ok(success),
+                Err(e) => match error {
+                    Some(ref mut prev) => {
+                        prev.extend(e);
+                    }
+                    None => error = Some(e),
+                },
+            }
+        }
+
+        Err(error.expect("Config should never have less than one parser, this is a bug"))
+    }
+}
+
+impl Parser for Config {
+    fn parse_str(&self, input: &str) -> Result<InputType, InputError> {
+        self.internal_parse(|p| p.parse_str(input))
+    }
+
+    fn parse_os_str(&self, input: &OsStr) -> Result<InputType, InputError> {
+        self.internal_parse(|p| p.parse_os_str(input))
+    }
+
+    fn parse_bytes(&self, input: &[u8]) -> Result<InputType, InputError> {
+        self.internal_parse(|p| p.parse_bytes(input))
+    }
+}
+
+impl fmt::Debug for Config {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut dbg = f.debug_struct("Valid");
+
+        if let Some(text) = &self.inner.text {
+            dbg.field("text", &text);
+        }
+
+        if let Some(stdin) = &self.inner.stdin {
+            dbg.field("stdin", &stdin);
+        }
+
+        if let Some(file) = &self.inner.file {
+            dbg.field("file", &file);
+        }
+
+        dbg.finish()
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        let cfg = Builder::new().with(|b| b.text().stdin().file());
+
+        debug_assert!(cfg.is_valid());
+
+        Self { inner: cfg }
+    }
+}
--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,10 @@
 use std::{convert::TryFrom, fmt, io, str::FromStr};
 
-use crate::{error::InputParseError, parsers::InputType};
+use crate::{
+    builder::{Builder, Config},
+    error::{access::AccessError, input::InputError},
+    parsers::{InputType, Parser},
+};
 
 #[derive(Debug)]
 pub struct Input {
@@ -8,25 +12,34 @@ pub struct Input {
 }
 
 impl Input {
-    pub fn try_parse(s: impl AsRef<str>) -> Result<Self, InputParseError> {
-        let kind = InputType::try_from(s.as_ref())?;
+    pub fn builder() -> Builder {
+        Builder::default()
+    }
 
-        Ok(Self { kind })
+    pub fn with_defaults(input: impl AsRef<str>) -> Result<Self, InputError> {
+        Config::default()
+            .parse_str(input.as_ref())
+            .map(|i| Self::from_input_type(i))
     }
 
     pub fn access(&self) -> Result<InputReader, AccessError> {
         Read::try_from(&self.kind).map(|r| InputReader::new(r))
     }
-}
 
-impl FromStr for Input {
-    type Err = InputParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Self::try_parse(s)
+    pub(crate) fn from_input_type(i: InputType) -> Self {
+        Self { kind: i }
     }
 }
 
+impl FromStr for Input {
+    type Err = InputError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::with_defaults(s)
+    }
+}
+
+#[derive(Debug)]
 pub struct InputReader {
     input: Read,
 }
@@ -44,17 +57,24 @@ impl io::Read for InputReader {
 }
 
 enum Read {
-    F(std::fs::File),
-    S(std::io::Stdin),
+    File(std::fs::File),
+    Stdin(std::io::Stdin),
+    Text(io::Cursor<String>),
 }
 
 impl Read {
     fn stdin() -> Self {
-        Self::S(io::stdin())
+        Self::Stdin(io::stdin())
     }
 
     fn file(f: std::fs::File) -> Self {
-        Self::F(f)
+        Self::File(f)
+    }
+
+    fn text(s: impl AsRef<str>) -> Self {
+        let s = s.as_ref().to_string();
+
+        Self::Text(io::Cursor::new(s))
     }
 }
 
@@ -63,10 +83,11 @@ impl TryFrom<&InputType> for Read {
 
     fn try_from(kind: &InputType) -> Result<Self, Self::Error> {
         match kind {
-            InputType::Stdin(_) => Ok(Read::stdin()),
+            InputType::Stdin => Ok(Read::stdin()),
             InputType::File(ref f) => std::fs::File::open(f.path.as_path())
                 .map(|f| Read::file(f))
-                .map_err(|_e| AccessError::default()),
+                .map_err(|e| AccessError::file_with_context(e, f.path.as_path())),
+            InputType::UTF8(ref s) => Ok(Self::text(s)),
         }
     }
 }
@@ -75,22 +96,24 @@ impl io::Read for Read {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         use Read::*;
         match self {
-            F(ref mut file) => io::Read::read(file, buf),
-            S(ref mut stdin) => io::Read::read(stdin, buf),
+            File(ref mut file) => io::Read::read(file, buf),
+            Stdin(ref mut stdin) => io::Read::read(stdin, buf),
+            Text(ref mut cursor) => io::Read::read(cursor, buf),
         }
     }
 }
 
-#[derive(Debug, Default)]
-pub struct AccessError;
-
-impl fmt::Display for AccessError {
+impl fmt::Debug for Read {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
-            f,
-            "hey idiot maintainer, you should probably fix this error"
-        )
+        use Read::*;
+        let mut dbg = f.debug_struct("Read");
+
+        match self {
+            File(f) => dbg.field("file", &f),
+            Stdin(s) => dbg.field("stdin", &s),
+            Text(t) => dbg.field("cursor", &t),
+        };
+
+        dbg.finish()
     }
 }
-
-impl std::error::Error for AccessError {}
